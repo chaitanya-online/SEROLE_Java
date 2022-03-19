13346ba38bf74f6f664bd044bd9a26743f1c5cca
Chayn <Chayn@Chaitanya-Nampalli>
2022-03-19 11:09:44 +0530

    Hello Class using Scaner Class

---
+++ SEROLE_Java/src/JDBC/Hello.java
@@ -0,0 +1,11 @@
+package JDBC;
+import java.util.*;
+public class Hello {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String name=sc.nextLine();
+		System.out.println("You Entered : -"+name);
+		
+	}
+
+}
