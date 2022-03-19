4049c67316e584e92120cdcacf54d9ec18324fd2
Chayn <Chayn@Chaitanya-Nampalli>
2022-03-19 11:10:59 +0530

    Added new line no 5

--- SEROLE_Java/src/JDBC/Hello.java
+++ SEROLE_Java/src/JDBC/Hello.java
@@ -2,6 +2,7 @@ package JDBC;
 import java.util.*;
 public class Hello {
 	public static void main(String[] args) {
+		System.out.println("Enter String");
 		Scanner sc=new Scanner(System.in);
 		String name=sc.nextLine();
 		System.out.println("You Entered : -"+name);
